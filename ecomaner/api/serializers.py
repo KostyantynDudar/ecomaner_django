1d080f006812aa2d889dcfb97b69e9cc50bd566e
KostyantynDudar <ecomaner@gmail.com>
2024-11-04 00:09:00 +0200

    Добавлена проверка уникальности email при регистрации с пользовательским сообщением

--- ecomaner/api/serializers.py
+++ ecomaner/api/serializers.py
@@ -6,3 +6,25 @@ class ExampleModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = ExampleModel  # Используйте вашу модель ExampleModel
         fields = '__all__'  # Укажите конкретные поля, если необходимо
+
+
+# api/serializers.py
+from django.contrib.auth.models import User
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('id', 'email', 'password')
+
+    def create(self, validated_data):
+        user = User(
+            email=validated_data['email']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+
--- ecomaner/api/urls.py
+++ ecomaner/api/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
 from .views import PhotoLocationsView  # Убедитесь, что этот импорт присутствует
+from .views import RegisterEmailView  # Убедитесь, что RegisterEmailView импортирован
+from .views import RegisterEmailView, LoginView  # Добавьте LoginView
 
 urlpatterns = [
     path('example/', views.ExampleView.as_view(), name='example'),
     path('photo-locations/', PhotoLocationsView.as_view(), name='photo_locations'),
+    path('register-email/', RegisterEmailView.as_view(), name='register-email'),
+    path('login/', LoginView.as_view(), name='login'),
 ]
