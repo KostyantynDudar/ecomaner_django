18369562ec86e518fdcb1db05c4ecfa28e32a9b4
KostyantynDudar <ecomaner@gmail.com>
2025-02-06 22:41:46 +0200

    Added date_added and created_by fields to LocationAdmin in Django Admin panel.

--- backend/apps/eco_map/admin.py
+++ backend/apps/eco_map/admin.py
@@ -3,7 +3,7 @@ from .models import Location
 
 @admin.register(Location)
 class LocationAdmin(admin.ModelAdmin):
-    list_display = ('id', 'latitude', 'longitude', 'type', 'status', 'address', 'size')
+    list_display = ('id', 'latitude', 'longitude', 'type', 'status', 'address', 'size', 'date_added', 'created_by')
     search_fields = ('description', 'address')
     list_filter = ('type', 'status')
     readonly_fields = ('date_added', 'last_updated')
